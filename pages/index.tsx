82b01345234e3849971275632ae8760b13a9a0d0
wakkihaya <wakkihaya@gmail.com>
2022-06-22 12:54:03 +0900

    wip: grid template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
-
+        <div className="container__title">Neurotech Daily</div>
+        <div className="container__cta">CTA</div>
+        <div className="container__description">Description</div>
+        <div className="container__image">Image</div>
+        <div className="container__about-us">NeurotechJP</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
