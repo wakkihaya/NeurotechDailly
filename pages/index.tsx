adbbf7ad333903917f71dc8819dd09576ac4275c
wakkihaya <wakkihaya@gmail.com>
2022-06-24 18:56:21 +0900

    update: grid column

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,23 @@ const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <div className="container__title">Neurotech Daily</div>
-        <div className="container__cta">CTA</div>
-        <div className="container__description">Description</div>
-        <div className="container__image">Image</div>
-        <div className="container__about-us">NeurotechJP</div>
+        <div className="container__left">
+          <div className="container__title">
+            <div className="container__title__name">
+              {" "}
+              Neurotech <br /> Daily
+            </div>
+            <div className="container__title__podcast-btn">
+              Listen on Podcast
+            </div>
+          </div>
+          <div className="container__cta">CTA</div>
+        </div>
+        <div className="container__right">
+          <div className="container__description">Description</div>
+          <div className="container__image">Image</div>
+          <div className="container__about-us">NeurotechJP</div>
+        </div>
       </div>
     </div>
   );
