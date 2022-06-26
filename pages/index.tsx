72cce773359264dd5f3cd9827156e154e76f3ded
wakkihaya <wakkihaya@gmail.com>
2022-06-26 11:06:03 +0200

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
         <div className="container__left">
-          <div className="container__title">
-            <div className="container__title__name">
-              {" "}
-              Neurotech <br /> Daily
-            </div>
-            <div className="container__title__podcast-btn">
-              Listen on Podcast
+          <div className="container__cover container__cover--title">
+            <div className="container__title">
+              <div className="container__title__name">
+                Neurotech <br /> Daily
+              </div>
+              <div className="container__title__podcast-btn">
+                Listen on Podcast
+              </div>
             </div>
           </div>
-          <div className="container__cta">CTA</div>
+          <div className="container__cover container__cover--cta">
+            <div className="container__cta">
+              <div className="container__cta__item">
+                Apple Podcast{" "}
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </div>
+              <div className="container__cta__item">
+                Google Podcast{" "}
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </div>
+              <div className="container__cta__item">
+                Spotify <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="container__right">
-          <div className="container__description">Description</div>
-          <div className="container__image">Image</div>
-          <div className="container__about-us">NeurotechJP</div>
+          <div className="container__cover container__cover--description">
+            <div className="container__description">
+              Stay up-to-date <br /> with the latest neurotech. <br />{" "}
+              neuroscience x technology
+            </div>
+          </div>
+          <div className="container__cover">
+            <img className="container__image" src="brain.png" alt="brain" />
+          </div>
+          <div className="container__cover container__cover--about-us">
+            <div className="container__about-us">NeurotechJP</div>
+          </div>
         </div>
       </div>
     </div>
