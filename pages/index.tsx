f0115dfe185708307078cbc2dc98a23033f1fec7
wakkihaya <wakkihaya@gmail.com>
2022-07-19 11:40:19 +0900

    fix: height issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { HomeDesktop, HomeMobile } from "../components/Home";
 
 const App = () => {
+  const [mounted, setMounted] = useState(false);
+
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
-  return <>{isTabletOrMobile ? <HomeMobile /> : <HomeDesktop />}</>;
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <></>;
+  }
+
+  if (isTabletOrMobile) {
+    return <HomeMobile />;
+  }
+  return <HomeDesktop />;
 };
 
 export default App;
