227c434977a41aad45faca8f691cf81b6fd9e285
wakkihaya <wakkihaya@gmail.com>
2022-07-24 15:01:02 +0900

    update: ogp title description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,30 @@ export default class MyDocument extends Document<WithNonceProp> {
           />
           <link rel="canonical" href="https://neurotechdaily.com" />
           <title>Neurotech Daily</title>
+          <meta name="copyright" content="Neurotech Daily" />
+          <meta
+            name="description"
+            content="Neurotech Daily, a Podcast that interview the cutting-edge founders, developers, and innovators in the neurotech space."
+          />
+          <meta property="og:type" content="website" />
+          <meta name="og:title" property="og:title" content="Neurotech Daily" />
+          <meta
+            name="og:description"
+            property="og:description"
+            content="Neurotech Daily, a Podcast that interview the cutting-edge founders, developers, and innovators in the neurotech space."
+          />
+          <meta
+            name="keywords"
+            content="neurotech,neurotechjp,braintech,neurotechnology,interview,neurotechdaily"
+          />
+          <meta property="og:site_name" content="Neurotech Daily" />
+          <meta property="og:url" content="https://neurotechdaily.com" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Neurotech Ddaily" />
+          <meta
+            name="twitter:description"
+            content="Neurotech Daily, a Podcast that interview the cutting-edge founders, developers, and innovators in the neurotech space."
+          />
         </Head>
         <body>
           <Main />
